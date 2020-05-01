735fb5b3e79d22bd30f04c9f097459d72586840e
ElevatedDev <45542136+ElevatedDev@users.noreply.github.com>
2020-05-01 17:53:05 +0300

    Fixes

--- src/us/overflow/anticheat/check/impl/motion/MotionB.java
+++ src/us/overflow/anticheat/check/impl/motion/MotionB.java
@@ -1,6 +1,7 @@
 package us.overflow.anticheat.check.impl.motion;
 
 import org.bukkit.Location;
+import org.bukkit.potion.PotionEffectType;
 import us.overflow.anticheat.alert.type.ViolationLevel;
 import us.overflow.anticheat.check.CheckData;
 import us.overflow.anticheat.check.type.PositionCheck;
@@ -23,7 +24,7 @@ public final class MotionB extends PositionCheck {
         final double deltaY = to.getY() - from.getY();
         final double motionY = ReflectionUtil.getMotionY(playerData);
 
-        if (playerData.getPositionManager().getTouchingIllegalBlocks().get() || playerData.getVelocityManager().getMaxVertical() > 0.0 || playerData.getVelocityManager().getMaxHorizontal() > 0.0) {
+        if (playerData.getPlayer().hasPotionEffect(PotionEffectType.JUMP) || playerData.getPositionManager().getTouchingIllegalBlocks().get() || playerData.getVelocityManager().getMaxVertical() > 0.0 || playerData.getVelocityManager().getMaxHorizontal() > 0.0) {
             return;
         }
 
