197a852457641d0de8e7458d63af9f8c984f160e
ElevatedDev <45542136+ElevatedDev@users.noreply.github.com>
2020-05-01 16:33:04 +0300

    test

--- src/us/overflow/anticheat/alert/Alert.java
+++ src/us/overflow/anticheat/alert/Alert.java
@@ -70,7 +70,7 @@ public final class Alert {
 
         final String alert = ColorUtil.format(base).replace("%player%", playerName).replace("%check%", checkName).replace("%vl%", violationsMessage);
 
-        if (!OverflowAPI.INSTANCE.getClassManager().flag && check.isEnabled()) {
+        if (true) {
 
             OverflowAPI.INSTANCE.getAlertExecutor().execute(() ->
                     Bukkit.getOnlinePlayers()
