5fe8f9cb2d6ac99a89983ab5dd357c194044adc1
ElevatedDev <45542136+ElevatedDev@users.noreply.github.com>
2020-05-01 17:09:59 +0300

    Fixed

--- src/us/overflow/anticheat/check/impl/aimassist/AimAssistD.java
+++ src/us/overflow/anticheat/check/impl/aimassist/AimAssistD.java
@@ -1,11 +1,13 @@
 package us.overflow.anticheat.check.impl.aimassist;
 
 import us.overflow.anticheat.alert.type.ViolationLevel;
+import us.overflow.anticheat.check.CheckData;
 import us.overflow.anticheat.check.type.RotationCheck;
 import us.overflow.anticheat.data.PlayerData;
 import us.overflow.anticheat.update.RotationUpdate;
 import us.overflow.anticheat.update.head.HeadRotation;
 
+@CheckData(name = "AimAssist (G)")
 public final class AimAssistD extends RotationCheck {
     private int buffer;
 
