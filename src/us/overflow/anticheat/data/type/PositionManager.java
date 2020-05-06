fd1162daa7dc37b13d0d5b7b1204d8d64319556c
ElevatedDev <45542136+ElevatedDev@users.noreply.github.com>
2020-05-06 16:44:19 +0300

    Fixed a problem with the threads

--- src/us/overflow/anticheat/data/type/PositionManager.java
+++ src/us/overflow/anticheat/data/type/PositionManager.java
@@ -40,32 +40,29 @@ public final class PositionManager {
         this.touchingGround.set(false);
 
         // Running on a diff thread to minimize load
-        OverflowAPI.INSTANCE.getPositionExecutor().execute(() -> {
-            final boolean touchingAir = cuboid.checkBlocks(to.getWorld(), material -> material == Material.AIR);
-            final boolean touchingClimbable = cuboid.checkBlocks(to.getWorld(), material -> material == Material.VINE || material == Material.LADDER);
-            final boolean touchingLiquid = cuboid.checkBlocks(to.getWorld(), material -> material == Material.LAVA || material == Material.WATER || material == Material.STATIONARY_LAVA || material == Material.STATIONARY_WATER);
-            final boolean touchingHalfBlocks = cuboid.checkBlocks(to.getWorld(), material -> material.getData() == Stairs.class || material.getData() == Step.class || material.getData() == WoodenStep.class);
-            final boolean touchingIllegalBlocks = cuboid.checkBlocks(to.getWorld(), material -> material == Material.DAYLIGHT_DETECTOR || material == Material.DAYLIGHT_DETECTOR_INVERTED || material.getData() == Skull.class || material.getData() == FlowerPot.class || material == Material.CARPET || material == Material.CAULDRON || material == Material.WATER_LILY || material == Material.WEB || material.name().equals("SLIME_BLOCK"));
-            final boolean touchingFence = cuboid.checkBlocks(to.getWorld(), material -> material == Material.FENCE || material == Material.FENCE_GATE);
-            final boolean touchingDoor = cuboid.checkBlocks(to.getWorld(), material -> material.getData() == Gate.class);
-            final boolean belowBlocks = BlockUtil.isOnGround(to, -2);
-            final boolean touchingGround = BlockUtil.isOnGround(to, 1) || BlockUtil.isOnGround(to, 2);
-            final boolean touchingSlab = cuboid.checkBlocks(to.getWorld(), material -> material == Material.DOUBLE_STONE_SLAB2 || material == Material.STONE_SLAB2);
-            final boolean touchingSlime = cuboid.checkBlocks(to.getWorld(), material -> material == Material.SLIME_BLOCK);
+        final boolean touchingAir = cuboid.checkBlocks(to.getWorld(), material -> material == Material.AIR);
+        final boolean touchingClimbable = cuboid.checkBlocks(to.getWorld(), material -> material == Material.VINE || material == Material.LADDER);
+        final boolean touchingLiquid = cuboid.checkBlocks(to.getWorld(), material -> material == Material.LAVA || material == Material.WATER || material == Material.STATIONARY_LAVA || material == Material.STATIONARY_WATER);
+        final boolean touchingHalfBlocks = cuboid.checkBlocks(to.getWorld(), material -> material.getData() == Stairs.class || material.getData() == Step.class || material.getData() == WoodenStep.class);
+        final boolean touchingIllegalBlocks = cuboid.checkBlocks(to.getWorld(), material -> material == Material.DAYLIGHT_DETECTOR || material == Material.DAYLIGHT_DETECTOR_INVERTED || material.getData() == Skull.class || material.getData() == FlowerPot.class || material == Material.CARPET || material == Material.CAULDRON || material == Material.WATER_LILY || material == Material.WEB || material.name().equals("SLIME_BLOCK"));
+        final boolean touchingFence = cuboid.checkBlocks(to.getWorld(), material -> material == Material.FENCE || material == Material.FENCE_GATE);
+        final boolean touchingDoor = cuboid.checkBlocks(to.getWorld(), material -> material.getData() == Gate.class);
+        final boolean belowBlocks = BlockUtil.isOnGround(to, -2);
+        final boolean touchingGround = BlockUtil.isOnGround(to, 1) || BlockUtil.isOnGround(to, 2);
+        final boolean touchingSlab = cuboid.checkBlocks(to.getWorld(), material -> material == Material.DOUBLE_STONE_SLAB2 || material == Material.STONE_SLAB2);
+        final boolean touchingSlime = cuboid.checkBlocks(to.getWorld(), material -> material == Material.SLIME_BLOCK);
 
-
-            this.touchingSlime.set(touchingSlime);
-            this.touchingSlab.set(touchingSlab);
-            this.touchingGround.set(touchingGround);
-            this.touchingAir.set(touchingAir);
-            this.touchingLiquid.set(touchingLiquid);
-            this.touchingClimbable.set(touchingClimbable);
-            this.touchingHalfBlocks.set(touchingHalfBlocks);
-            this.touchingIllegalBlocks.set(touchingIllegalBlocks);
-            this.touchingFence.set(touchingFence);
-            this.touchingDoor.set(touchingDoor);
-            this.belowBlocks.set(belowBlocks);
-        });
+        this.touchingSlime.set(touchingSlime);
+        this.touchingSlab.set(touchingSlab);
+        this.touchingGround.set(touchingGround);
+        this.touchingAir.set(touchingAir);
+        this.touchingLiquid.set(touchingLiquid);
+        this.touchingClimbable.set(touchingClimbable);
+        this.touchingHalfBlocks.set(touchingHalfBlocks);
+        this.touchingIllegalBlocks.set(touchingIllegalBlocks);
+        this.touchingFence.set(touchingFence);
+        this.touchingDoor.set(touchingDoor);
+        this.belowBlocks.set(belowBlocks);
     }
 }
 
